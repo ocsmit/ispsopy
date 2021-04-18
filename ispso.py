d0c149ceaebc9fb387e1027d82f8a728d687724c
ocsmit <ocsmit@protonmail.com>
2021-04-18 15:56:58 -0400

    WIP

--- ispso.py
+++ ispso.py
@@ -1,12 +1,9 @@
 import numpy as np
-
-def main(s):
-    diversity = mean_diversity = []
-    evals = iter = 0
-    num_exclusions_per_nest = []
-    num_exclusions = 0
+import random
+from scipy.stats.qmc import Sobol
 
 
+# Subroutines
 def plotswarm(s):
     na = ["", "profile", "diversity", "mean_diversity", 1]
     if s.get("plot_method") in na:
@@ -59,25 +56,96 @@ def myround(x, digits=0):
     return np.around(x, digits)
 
 
-# Subroutines
 class ispso:
     def __init__(self, s):
-        self.best = float('inf')
+
+        if s.get("deterministic") is None:
+            s["deterministic"] = False
+        if s.get("stop_after_solutions") is None:
+            s["stop_after_solutions"] = 0
+        if s.get("plot_distance_to_solution") is None:
+            s["plot_distance_to_solution"] = 0.05
+        if s.get("plot_method") is None:
+            s["plot_method"] = "movement"
+        if s.get("plot_x") is None:
+            s["plot_x"] = [1, 2]
+        if s.get("plot_delay") is None:
+            s["plot_delay"] = 0
+        if s.get("plot_save_prefix") is None:
+            s["plot_save_prefix"] = ""
+
+        if s.get("S") < 2:
+            raise Exception("Swarm size must be greater than 1")
+
+        if s.get("plot_save_prefix") == "":
+            _plot_save_format = ""
+        else:
+            _plot_save_format = (
+                f"{s.get('plot_save_prefix')}"
+                "{np.floor(np.log10{s.get('maxiter')+1)}"
+            )
+
+        if s.get("dont_stop") is None:
+            s["dont_stop"] = False
+
+        if s.get("c1") is None:
+            s["c1"] = 2.05
+        if s.get("c2") is None:
+            s["c2"] = 2.05
+        if s.get("w") is None:
+            s["w"] = 2 / np.abs(
+                2
+                - s.get("c1")
+                - s.get("c2")
+                - np.sqrt(
+                    (s.get("c1") + s.get("c2")) ** 2 - 4 *
+                    (s.get("c1") + s.get("c2"))
+                )
+            )
+
+        self.s = s
+
+        self.best = float("inf")
         self.worst = -self.best
-        self.pbest = np.zeros([s.get('S'), s.get('D') + 1])
+
+        if s.get('deterministic'):
+            if 'seed_sobol' not in locals():
+                seed_sobol = 4711
+            if 'seed_random' in locals():
+                Random_seed = self.seed_random
+            else:
+                random.seed(0)
+                self.seed_random = [random.uniform(-1, 1)
+                                    for i in range(626)]
+        else:
+            seed_sobol = int(random.uniform(0, 1)*100000)
+            seed_random = [random.uniform(-1, 1)
+                           for i in range(626)]
+
+        pop = []
+        if len(pop) == 0:
+            x = self.new_x(s.get("S"), seed_sobol)
+
+
+
+        self.pbest = np.zeros([s.get("S"), s.get("D") + 1])
         self.gb = 0
-        self.gbest = np.zeros([s.get('D') + 1])
-        self.gbest[s.get('D')] = self.best
-        self.pbest[:, [s.get('D')]] = self.best
+        self.gbest = np.zeros([s.get("D") + 1])
+        self.gbest[s.get("D")] = self.best
+        self.pbest[:, [s.get("D")]] = self.best
 
         self.prev_gbestf = self.worst
         self.f = []
-        self.age = np.repeat(0, s.get('S'))
+        self.age = np.repeat(0, s.get("S"))
         self.V = []
-        self.halflife_age = myround(0.5*s.get('age'))
+        self.halflife_age = myround(0.5 * s.get("age"))
 
         self.seed = []
         self.species = []
+
+        pop = []
+        if pop is None:
+            x = self.new_x(s.get("S"), seed.get("sobol"))
 
     def start(self):
         diversity = mean_diversity = []
@@ -85,10 +153,22 @@ class ispso:
         num_exclusions_per_nest = []
         num_exclusions = 0
 
+        # while True:
+        #    iter += 1
+        #    diversity[iter] = np.mean(np.sqrt((x.T -  np.amin(x, axis=0)).T))
 
+    def new_x(self, n=1, seed=-1):
+        if seed >= 0:
+            r = Sobol(self.s.get("D"), True, seed).random(n)
+        else:
+            r = Sobol(self.s.get("D"), True).random(n)
 
+        return r
 
+    def evaluate_f(self, s):
 
+        f = []
+        for i in range(1, self.S):
+            f[i] = s.get("F")
 
-
-
+        return
