8b85d2782b995b356f9fab6ccfed80f1f9ffe312
ocsmit <ocsmit@protonmail.com>
2021-05-03 13:19:48 -0400

    Split testing code

--- ispso.py
+++ ispso.py
@@ -7,6 +7,7 @@ from scipy.stats.qmc import Sobol
 np.seterr(divide='ignore')
 np.warnings.filterwarnings('ignore', category=np.VisibleDeprecationWarning)
 
+
 class ispso_parameters:
 
     # Double type vector
@@ -55,39 +56,8 @@ class ispso_parameters:
 
 
 # Functions
-def f4(x, sol=False):
-    if sol:
-        return np.array([[0.08], [0.246], [0.449], [0.679], [0.93]])
-    else:
-        return (
-            1
-            - math.exp(-2 * math.log(2) * ((x - 0.08) / 0.854) ** 2)
-            * math.sin(5 * math.pi * (x ** (3 / 4) - 0.05)) ** 6
-        )
-
-
 def diagonal(s):
     return sum((s.xmax - s.xmin)**2)**0.5
-
-
-# TESTING PARAMETERS
-s = ispso_parameters()
-s.f = f4
-s.D = 1
-s.xmin = np.array([0])
-s.xmax = np.array([1])
-s.S = 10 + math.floor(2 * (s.D)**0.5)
-s.vmax = (s.xmax - s.xmin) * 0.1
-s.vmax0 = diagonal(s)*0.001
-s.exclusion_factor = 3
-s.maxiter = 200
-s.xeps = 0.001
-s.feps = 0.0001
-s.rprey = diagonal(s) * 0.0001
-s.age = 10
-s.rspecies = diagonal(s) * 0.1
-s.rnest = diagonal(s) * 0.1
-s.plot_distance_to_solution = 0.01
 
 
 # Subroutines
@@ -211,7 +181,7 @@ class ispso:
         self.seed = []
         self.species = []
 
-    def start(self):
+    def run(self):
         diversity = mean_diversity = []
         self.evals = self.iter = 0
         self.num_exclusions_per_nest = []
@@ -226,6 +196,11 @@ class ispso:
             if check or (self.s.maxiter and self.iter == self.s.maxiter):
                 break
             self.update_x()
+
+        return {'iter': self.iter,
+                'evals': self.evals,
+                'nest': self.nest,
+                'pop': self.pop}
 
     def update_x(self):
         self.x = (self.x + self.v).to_numpy()
@@ -506,5 +481,3 @@ class ispso:
         return False
 
 
-r = ispso(s)
-r.start()
---
+++ testing.py
@@ -0,0 +1,37 @@
+from ispso import ispso_parameters, ispso, diagonal
+import numpy as np
+import math
+
+# Functions
+def f4(x, sol=False):
+    if sol:
+        return np.array([[0.08], [0.246], [0.449], [0.679], [0.93]])
+    else:
+        return (
+            1
+            - math.exp(-2 * math.log(2) * ((x - 0.08) / 0.854) ** 2)
+            * math.sin(5 * math.pi * (x ** (3 / 4) - 0.05)) ** 6
+        )
+
+# TESTING PARAMETERS
+s = ispso_parameters()
+s.f = f4
+s.D = 1
+s.xmin = np.array([0])
+s.xmax = np.array([1])
+s.S = 10 + math.floor(2 * (s.D)**0.5)
+s.vmax = (s.xmax - s.xmin) * 0.1
+s.vmax0 = diagonal(s)*0.001
+s.exclusion_factor = 3
+s.maxiter = 200
+s.xeps = 0.001
+s.feps = 0.01
+s.rprey = diagonal(s) * 0.0001
+s.age = 10
+s.rspecies = diagonal(s) * 0.1
+s.rnest = diagonal(s) * 0.1
+s.plot_distance_to_solution = 0.01
+
+
+r = ispso(s).run()
+print(r.get('nest'))
